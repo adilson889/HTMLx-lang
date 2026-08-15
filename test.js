098710e2fa68c55c5f3b16648b02876c3cff34ec
Adilson C. Rafael <adilsonrafael847@gmail.com>
2026-08-15 16:10:51 +0100

    Excluir test.js

--- test.js
+++
@@ -1,5 +0,0 @@
-// test.js - Basic XLang test
-const { XLangInterpreter } = require('./xlang-interpreter.js');
-
-console.log('XLang Interpreter loaded successfully');
-console.log('All tests passed');
